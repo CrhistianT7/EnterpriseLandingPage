fa96a3e45ec81b90d565e30ee09eb47477afb359
Crhistian Turpo <cturpo@zagile.com>
2023-09-13 00:29:39 -0600

    feat: section and container ui components

---
+++ src/ui/Section/section.styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+interface IStyledSection {
+  type: "margin" | "padding";
+  size: "sm" | "md" | "lg" | "xl";
+}
+
+const sizeStyles = (size: string) => {
+  switch (size) {
+    case "sm":
+      return `32px`;
+    case "md":
+      return `48px`;
+    case "lg":
+      return `96px`;
+    case "xl":
+      return `144px`;
+    default:
+      break;
+  }
+};
+
+export const StyledSection = styled.div<IStyledSection>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 1280px;
+
+  ${(props) =>
+    props.type === "margin"
+      ? `margin: ${sizeStyles(props.size)}`
+      : `padding: ${sizeStyles(props.size)}`}
+`;
