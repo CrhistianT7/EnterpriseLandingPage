4e70dee2ff0debcd8d7854eb2bf8b309bc20ae40
Crhistian Turpo <65095486+CrhistianT7@users.noreply.github.com>
2023-09-13 20:31:18 -0500

    feat: Intl hook for translation & basic structure of the page (#2)
    Co-authored-by: Crhistian Turpo <cturpo@zagile.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import * as React from "react";
-import Section from "./ui/Section/Section";
+import React, { useState } from "react";
+import { IntlProvider } from "react-intl";
+import en from "../src/lang/en.json";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
+  const [locale] = useState<string>("es");
   return (
-    <>
-      <Section type="padding" size="md">hellod</Section>
-    </>
+    <IntlProvider locale={locale} messages={en}>
+      <>
+        <nav>navbar</nav>
+        <Hero />
+        <section>section 2</section>
+        <section>Portafolio / clients / parterns</section>
+        <section>Section 3</section>
+        <section>Tutoriales o cosas así</section>
+        <section>Contratar por 6 meses o un año, para dar experiencia</section>
+        <section>contact us</section>
+        <footer>footer</footer>
+      </>
+    </IntlProvider>
   );
 };
 
