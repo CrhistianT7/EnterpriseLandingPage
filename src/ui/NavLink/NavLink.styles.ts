78175bbe9f540f6b3bd3933a3ba41bccfa04cf76
Crhistian Turpo <andrewturpo573@gmail.com>
2023-12-08 11:49:52 -0700

    feat: navlink component completed

--- src/ui/NavLink/NavLink.styles.ts
+++ src/ui/NavLink/NavLink.styles.ts
@@ -1,36 +1,24 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import { colors } from "styles/colors";
 
 export const StyledListItem = styled.li`
   list-style-type: none;
-  padding: .25rem .25rem;
 `;
-
-const borderBottomAnimation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`
 
 export const StyledAnchor = styled.a`
   color: ${colors.primary.body};
   text-decoration: none;
   font-size: 16px;
+  padding: .25rem .25rem;
 
   &:hover {
     border-bottom: 2px solid #d20427;
     border-radius: 2px;
-    animation: ${borderBottomAnimation} 2s linear infinite;
   }
 
   &:active, &:focus {
     outline: 1px solid #d20427;
     border-radius: 6px;
-    padding: 0 10px;
   }
 `;
