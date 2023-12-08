3df89f90021bc91aab6cc3c354a1325bd99e1c95
Gustavout22 <gustavougartetorres@gmail.com>
2023-12-08 11:02:50 -0500

    fix input

--- src/ui/input/Input.tsx
+++ src/ui/input/Input.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { InputWrapper, StyledInput, StyledLabel } from "./Input.styles";
+import { ChangeEvent } from "react";
+
+type Value = string | number | boolean | Date
 
 interface IStyledInput {
   id: string
   type?: "text" | "number" | "email" | "password" 
   name?: string
-  value: React.HTMLInputTypeAttribute
-  onChange?: React.ChangeEventHandler<HTMLInputElement>
+  value: Value
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
   placeholder?: string
   label?: string
   required?: boolean
@@ -29,7 +32,7 @@ const Input: React.FC<IStyledInput> = ({
         id={id || name}
         type={type}
         name={name}
-        value={value}
+        value={value as string}
         onChange={onChange}
         placeholder={placeholder}
         required={required}
