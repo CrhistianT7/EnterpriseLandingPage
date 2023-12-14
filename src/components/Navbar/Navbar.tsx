e3a12606e0150745480d1241c4a98099a5d4ce1d
Crhistian Turpo <andrewturpo573@gmail.com>
2023-12-14 13:37:27 -0700

    first version of responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react'
+import styled from 'styled-components'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
-import useIntlMessages from "../../hooks/useIntlMessages";
-import { breakpoints } from "../../shared/breakpoints";
-import Button from "../../ui/Button/Button";
-import NavLink from "../../ui/NavLink/NavLink";
-import LanguageSwitcher from "../../components/LanguageSwitcher/LanguageSwitcher";
+import useIntlMessages from '../../hooks/useIntlMessages'
+import { breakpoints } from '../../shared/breakpoints'
+import Button from '../../ui/Button/Button'
+import NavLink from '../../ui/NavLink/NavLink'
 
 const StyledNavbar = styled.nav`
   font-size: 16px;
   font-weight: 500;
-`;
+`
 
 const NavbarWrapper = styled.div`
   display: flex;
@@ -20,21 +19,17 @@ const NavbarWrapper = styled.div`
   max-width: 1024px;
   margin: auto;
   padding: 0 16px;
-
-  /* @media only screen and (min-width: ${breakpoints.xs}) {
-    max-width: 700px;
-  } */
-`;
+`
 
 const NavLinksWrapper = styled.div`
   display: none;
-  gap: .25rem;
+  gap: 0.25rem;
   align-items: center;
 
   @media only screen and (min-width: ${breakpoints.lg}) {
     display: flex;
   }
-`;
+`
 
 const NavLinksWrapperMobile = styled.div`
   display: flex;
@@ -44,18 +39,18 @@ const NavLinksWrapperMobile = styled.div`
   @media only screen and (min-width: ${breakpoints.lg}) {
     display: none;
   }
-`;
+`
 
 const StyledUnorderedList = styled.ul`
   display: flex;
   gap: 1rem;
-`;
+`
 
 const StyledUnorderedListMobile = styled.ul`
   display: flex;
   gap: 1rem;
   flex-direction: column;
-`;
+`
 
 const HamburgerIcon = styled.div`
   display: flex;
@@ -63,35 +58,35 @@ const HamburgerIcon = styled.div`
   @media only screen and (min-width: ${breakpoints.lg}) {
     display: none;
   }
-`;
+`
 
 export const Navbar: React.FC = (): React.ReactElement => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  const intl = useIntlMessages();
+  const intl = useIntlMessages()
 
   const navbarItems = [
     {
-      name: "Portafolio",
-      href: "#portafolio",
+      name: 'Portafolio',
+      href: '#portafolio',
     },
     {
-      name: "Learn with us",
-      href: "/",
+      name: intl('home.navbar.link.learn.with.us'),
+      href: 'learn-with-us',
     },
     {
-      name: "Services",
-      href: "/",
+      name: 'Services',
+      href: 'services',
     },
     {
-      name: "About us",
-      href: "/about-us",
+      name: intl('home.navbar.link.about.us'),
+      href: 'about-us',
     },
-  ];
+  ]
 
   const toggleNavbar = () => {
-    setIsOpen((prev) => !prev);
-  };
+    setIsOpen((prev) => !prev)
+  }
 
   return (
     <StyledNavbar>
@@ -103,14 +98,13 @@ export const Navbar: React.FC = (): React.ReactElement => {
             ))}
           </StyledUnorderedList>
           <Button type="primary">
-            {intl("home.navbar.button.contact.us")}
+            {intl('home.navbar.button.contact.us')}
           </Button>
-          <LanguageSwitcher />
         </NavLinksWrapper>
         <HamburgerIcon>
-          <button onClick={toggleNavbar}>
-            {isOpen ? "X" : <GiHamburgerMenu />}
-          </button>
+          <Button type="secondary" onClick={toggleNavbar}>
+            {isOpen ? 'X' : <GiHamburgerMenu />}
+          </Button>
         </HamburgerIcon>
       </NavbarWrapper>
       {isOpen && (
@@ -121,12 +115,12 @@ export const Navbar: React.FC = (): React.ReactElement => {
             ))}
           </StyledUnorderedListMobile>
           <Button type="primary">
-            {intl("home.navbar.button.contact.us")}
+            {intl('home.navbar.button.contact.us')}
           </Button>
         </NavLinksWrapperMobile>
       )}
     </StyledNavbar>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/ui/Container/Container.tsx
+++ src/ui/Container/Container.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { StyledContainer } from "./Container.styles";
+import { StyledContainer } from './Container.styles'
 
 interface IContainer {
-  size: "sm" | "md" | "lg" | "xl";
-  className?: string,
-  children: React.ReactNode;
+  size: 'sm' | 'md' | 'lg' | 'xl'
+  className?: string
+  children: React.ReactNode
 }
 
-const Container: React.FC<IContainer> = ({
-  size,
-  children,
-  className
-}) => {
-  return <StyledContainer size={size} className={className}>{children}</StyledContainer>;
-};
+const Container: React.FC<IContainer> = ({ size, children, className }) => {
+  return (
+    <StyledContainer size={size} className={className}>
+      {children}
+    </StyledContainer>
+  )
+}
 
-export default Container;
+export default Container
