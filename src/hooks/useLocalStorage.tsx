19d31336d44a7fc9c5f83a2d0884ef5d57de682f
Gustavout22 <gustavougartetorres@gmail.com>
2024-01-09 22:43:14 -0500

    add:useLocalStorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,23 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-type StoredValue<T> = T | (() => T);
+type StoredValue<T> = T | (() => T)
 
 function getSavedValue<T>(key: string, initialValue: StoredValue<T>): T {
-  const savedValue = JSON.parse(localStorage.getItem(key) || "null") as T | null;
-  if (savedValue !== null) return savedValue;
+  const savedValue = JSON.parse(localStorage.getItem(key) || 'null') as T | null
+  if (savedValue !== null) return savedValue
 
-  return initialValue instanceof Function ? initialValue() : initialValue;
+  return initialValue instanceof Function ? initialValue() : initialValue
 }
 
-export default function useLocalStorage<T>(key: string, initialValue: StoredValue<T>): [T, (newValue: T) => void] {
+export default function useLocalStorage<T>(
+  key: string,
+  initialValue: StoredValue<T>
+): [T, (newValue: T) => void] {
   const [value, setValue] = useState<T>(() => {
-    return getSavedValue<T>(key, initialValue);
-  });
+    return getSavedValue<T>(key, initialValue)
+  })
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [value, key]);
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value, key])
 
   return [value, setValue]
 }
-
