a7d45a4afb29b1a75e0c323c0fe32e490544f4b6
CrhistianT <andrewturpo573@gmail.com>
2024-01-12 09:29:42 -0500

    header/navbar

--- src/components/Navbar/Navbar.styles.ts
+++ src/components/Navbar/Navbar.styles.ts
@@ -1,3 +1,4 @@
+import { breakpoints } from "shared/breakpoints";
 import styled from "styled-components";
 import Container from "ui/Container/Container";
 
@@ -19,4 +20,43 @@ export const HeaderWrapper = styled(Container)`
   justify-content: space-between;
   position: relative;
   padding: 16px 16px;
+`
+
+export const HamburgerIcon = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  @media only screen and (min-width: ${breakpoints.lg}) {
+    display: none;
+  }
+`
+
+export const DesktopNavigation = styled.nav`
+  display: none;
+
+  @media only screen and (min-width: ${breakpoints.lg}) {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+`
+
+export const DesktopUnorderedList = styled.ul`
+  display: flex;
+  gap: .5rem;
+`
+
+export const MobileNavigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  border-top: 2px solid red;
+  width: 100%;
+  padding: .75rem;
+
+  transition: display 5s linear;
+
+  @media only screen and (min-width: ${breakpoints.lg}) {
+    display: none;
+  }
 `
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,20 @@
 import { useState } from 'react'
-import styled from 'styled-components'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { CgClose } from "react-icons/cg";
+import { CgClose } from 'react-icons/cg'
 
-import { breakpoints } from 'shared/breakpoints'
 import logo from '../../assets/images/logo/only-text.svg'
 import LanguageSwitcher from 'components/LanguageSwitcher/LanguageSwitcher'
 import useIntlMessages from 'hooks/useIntlMessages'
-import { HeaderWrapper, StyledHeader } from './Navbar.styles'
+import {
+  DesktopNavigation,
+  DesktopUnorderedList,
+  HamburgerIcon,
+  HeaderWrapper,
+  MobileNavigation,
+  StyledHeader,
+} from './Navbar.styles'
 import NavLink from 'ui/NavLink/NavLink'
 import Button from 'ui/Button/Button'
-
-const HamburgerIcon = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-
-  @media only screen and (min-width: ${breakpoints.lg}) {
-    display: none;
-  }
-`
-
-const DesktopNavigation = styled.nav`
-  display: none;
-
-  @media only screen and (min-width: ${breakpoints.lg}) {
-    display: flex;
-    gap: 10px;
-    align-items: center;
-  }
-`
-
-const DesktopUnorderedList = styled.ul`
-  display: flex;
-  gap: .5rem;
-`
-
-const MobileNavigation = styled.nav`
-  display: flex;
-  flex-direction: column;
-  border-top: 2px solid red;
-  width: 100%;
-  padding: .75rem;
-
-  @media only screen and (min-width: ${breakpoints.lg}) {
-    display: none;
-  }
-`
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -62,7 +30,7 @@ const Navbar: React.FC = () => {
       href: 'learn-with-us',
     },
     {
-      name: 'Services',
+      name: intl('home.navbar.link.services'),
       href: 'services',
     },
     {
@@ -83,7 +51,11 @@ const Navbar: React.FC = () => {
         </a>
         <HamburgerIcon>
           <LanguageSwitcher />
-          {isOpen ? <CgClose onClick={handleIsOpen} /> : <GiHamburgerMenu onClick={handleIsOpen}/>}
+          {isOpen ? (
+            <CgClose onClick={handleIsOpen} />
+          ) : (
+            <GiHamburgerMenu onClick={handleIsOpen} />
+          )}
         </HamburgerIcon>
         <DesktopNavigation>
           <DesktopUnorderedList>
@@ -91,7 +63,7 @@ const Navbar: React.FC = () => {
               <NavLink key={navItem.name} item={navItem} />
             ))}
           </DesktopUnorderedList>
-          <Button type="primary" onClick={() => console.log('hello')}>
+          <Button type="primary">
             {intl('home.navbar.button.contact.us')}
           </Button>
           <LanguageSwitcher />
@@ -105,7 +77,7 @@ const Navbar: React.FC = () => {
               <NavLink key={navItem.name} item={navItem} />
             ))}
           </ul>
-          <Button type="primary" onClick={() => console.log('hello')}>
+          <Button type="primary">
             {intl('home.navbar.button.contact.us')}
           </Button>
         </MobileNavigation>
