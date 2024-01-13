846225b3b5daf07405e4ddfa8f224fe0f63e27eb
CrhistianT <andrewturpo573@gmail.com>
2024-01-13 09:49:22 -0500

    approuter component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import { IntlProvider } from 'react-intl'
 import { GlobalStyle } from './styles/global'
 import { useLanguageContext } from 'context/Language/useLanguageContext'
-import { Routes, Route } from 'react-router-dom'
 
-import Home from 'pages/Home/Home'
-import AboutUs from 'pages/AboutUs/AboutUs'
-import PageNotFound from 'pages/PageNotFound/PageNotFound'
-import ContactUs from 'pages/ContactUs/ContactUs'
-import PrivacyPolicy from 'pages/PrivacyPolicy/PrivacyPolicy'
-import TermsOfService from 'pages/TermsOfService/TermsOfService'
-import Layout from 'components/Layout/Layout'
+import AppRouter from 'routes/AppRouter'
 
 const App = () => {
   const { language } = useLanguageContext()
@@ -17,16 +10,7 @@ const App = () => {
     <>
       <GlobalStyle />
       <IntlProvider locale={language.locale} messages={language.messages}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
+        <AppRouter/>
       </IntlProvider>
     </>
   )
