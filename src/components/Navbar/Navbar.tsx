4c63bb090755818612c31ee53da16b7a235adcd8
CrhistianT <andrewturpo573@gmail.com>
2024-01-24 15:37:21 -0500

    fix: improve and bug correction header

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { CgClose } from 'react-icons/cg'
-import { useNavigate } from 'react-router-dom'
+import { NavLink as ReactRouterNavLink } from 'react-router-dom'
 import logo from '../../assets/images/logo/only-text.svg'
 import LanguageSwitcher from 'components/LanguageSwitcher/LanguageSwitcher'
 import useIntlMessages from 'hooks/useIntlMessages'
@@ -15,35 +15,28 @@ import {
   StyledHeader,
 } from './Navbar.styles'
 import NavLink from 'ui/NavLink/NavLink'
-import Button from 'ui/Button/Button'
+import ButtonLink from 'ui/ButtonLink/ButtonLink'
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-
-  const navigate = useNavigate()
-
-  const navigateContactUs = () => {
-    navigate('/contact-us')
-  }
-
   const intl = useIntlMessages()
 
   const navbarItems = [
     {
       name: intl('home.navbar.link.portafolio'),
-      href: 'portafolio',
+      href: '/portafolio',
     },
     {
       name: intl('home.navbar.link.learn.with.us'),
-      href: 'learn-with-us',
+      href: '/learn-with-us',
     },
     {
       name: intl('home.navbar.link.services'),
-      href: 'services',
+      href: '/services',
     },
     {
       name: intl('home.navbar.link.about.us'),
-      href: 'about-us',
+      href: '/about-us',
     },
   ]
 
@@ -56,15 +49,15 @@ const Navbar: React.FC = () => {
       <HeaderEmptyBackground />
       <StyledHeader>
         <HeaderWrapper size="lg">
-          <a href="/">
+          <ReactRouterNavLink to={'/'}>
             <img src={logo} alt="Binland logo" />
-          </a>
+          </ReactRouterNavLink>
           <HamburgerIcon>
             <LanguageSwitcher />
             {isOpen ? (
-              <CgClose onClick={handleIsOpen} />
+              <CgClose onClick={handleIsOpen} cursor={"pointer"} />
             ) : (
-              <GiHamburgerMenu onClick={handleIsOpen} />
+              <GiHamburgerMenu onClick={handleIsOpen} cursor={"pointer"}/>
             )}
           </HamburgerIcon>
           <DesktopNavigation>
@@ -73,9 +66,9 @@ const Navbar: React.FC = () => {
                 <NavLink key={navItem.name} item={navItem} />
               ))}
             </DesktopUnorderedList>
-            <Button type="primary" onClick={navigateContactUs}>
+            <ButtonLink to="/contact-us" type="primary">
               {intl('home.navbar.button.contact.us')}
-            </Button>
+            </ButtonLink>
             <LanguageSwitcher />
           </DesktopNavigation>
         </HeaderWrapper>
@@ -84,12 +77,20 @@ const Navbar: React.FC = () => {
           <MobileNavigation>
             <ul>
               {navbarItems.map((navItem) => (
-                <NavLink key={navItem.name} item={navItem} />
+                <NavLink
+                  key={navItem.name}
+                  item={navItem}
+                  onClick={() => setIsOpen(false)}
+                />
               ))}
             </ul>
-            <Button type="primary" onClick={navigateContactUs}>
+            <ButtonLink
+              to="/contact-us"
+              type="primary"
+              onClick={() => setIsOpen(false)}
+            >
               {intl('home.navbar.button.contact.us')}
-            </Button>
+            </ButtonLink>
           </MobileNavigation>
         )}
       </StyledHeader>
