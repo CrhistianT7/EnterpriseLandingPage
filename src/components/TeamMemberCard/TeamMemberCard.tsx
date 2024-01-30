90513f8802c427cb8be48dd842b3b2807b3ecd97
RicardoC23 <ricardomamani403@gmail.com>
2024-01-29 21:25:53 -0500

    aboutUs progress

--- src/components/TeamMemberCard/TeamMemberCard.tsx
+++ src/components/TeamMemberCard/TeamMemberCard.tsx
@@ -1,15 +1,45 @@
-import styled from "styled-components"
+import styled from 'styled-components'
+import { FiGithub } from 'react-icons/fi'
+import { AiOutlineGlobal } from 'react-icons/ai'
 
 interface ITeamMemberCard {
   name: string
   position: string
-  quote?: string
+  quote: string
   image: string
+  description?: string
   website_url?: string
 }
 
 const StyledTeamMemberCard = styled.div`
   border: 1px solid red;
+`
+const ImgFounder = styled.img`
+  width: 170px;
+  height: 170px;
+`
+const NameFounder = styled.h4`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 36px;
+`
+const PositionFounder = styled.p`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 36px;
+  color: #f83d48;
+`
+const QuoteFounder = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #424242;
+`
+const LinkFounder = styled.div`
+  /* display: flex;
+  width: 19.5px;
+  height: 20.03px;
+*/
 `
 
 const TeamMemberCard: React.FC<ITeamMemberCard> = ({
@@ -21,12 +51,15 @@ const TeamMemberCard: React.FC<ITeamMemberCard> = ({
 }) => {
   return (
     <StyledTeamMemberCard>
-      <p>{image}</p>
-      <h4>{name}</h4>
-      <p>{position
-      }</p>
-      <p>{quote}</p>
-      <div>link to: {website_url}</div>
+      <ImgFounder src={image} alt="" />
+      <NameFounder>{name}</NameFounder>
+      <PositionFounder>{position}</PositionFounder>
+      <QuoteFounder>{quote}</QuoteFounder>
+      <LinkFounder>
+        link to: {website_url}
+        <FiGithub />
+        <AiOutlineGlobal />
+      </LinkFounder>
     </StyledTeamMemberCard>
   )
 }
--- src/pages/AboutUs/AboutUs.styles.ts
+++ src/pages/AboutUs/AboutUs.styles.ts
@@ -1,7 +1,8 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const MembersWrapper = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 15px;
 `
