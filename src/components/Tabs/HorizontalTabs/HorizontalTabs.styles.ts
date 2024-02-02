13e018cbfa6f455b7efb43182105343f57e96f63
Crhistian Turpo <andrewturpo573@gmail.com>
2024-02-02 17:58:49 -0500

    done: services section

---
+++ src/components/Tabs/HorizontalTabs/HorizontalTabs.styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components"
+import { colors } from "styles/colors"
+
+export const HorizontalTabsWrapper = styled.div`
+  width: 100%;
+`
+
+export const TabsWrapper = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: 0.0625rem;
+
+  :first-child {
+    border-top-left-radius: 1rem;
+  }
+
+  :last-child {
+    border-top-right-radius: 1rem;
+  }
+`
+
+export const SingleTab = styled.li`
+  list-style: none;
+  cursor: pointer;
+  padding: 0.625rem 1rem;
+  background: ${colors.secondary.main};
+  color: ${colors.white.main};
+  font-weight: 600;
+
+  &.selected {
+    background: ${colors.white.main};
+    margin: 0 0.625rem;
+    color: ${colors.primary.main};
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+  }
+`
+
+export const ContentWrapper = styled.div`
+  background: ${colors.white.main};
+  border-radius: 1rem;
+  padding: 2rem 1.5rem;
+`
