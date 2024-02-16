9e2202d7a21364402b0600afe60020904f80647e
RicardoC23 <ricardomamani403@gmail.com>
2024-02-15 22:26:57 -0500

    page not found was completed

--- src/pages/PageNotFound/PageNotFound.style.ts
+++ src/pages/PageNotFound/PageNotFound.style.ts
@@ -1,50 +1,55 @@
+import styled from 'styled-components'
+
 import { colors } from 'styles/colors'
 import { breakpoints } from 'shared/breakpoints'
-import styled from 'styled-components'
-import ButtonLink from 'ui/ButtonLink/ButtonLink'
 
-export const WrapperPageNotFound = styled.div`
+export const PageNotFoundWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   gap: 1.125rem;
   padding: 3rem 1.5rem;
+  margin: 3rem 0 3rem 0;
+
+  @media only screen and (min-width: ${breakpoints.lg}) {
+    margin: 2rem 0 1rem 0;
+  }
 
   .not-found-img {
     margin-bottom: 2.5rem;
   }
+
+  .button-not-found {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 600;
+  }
 `
-export const ContentExclamation = styled.div`
+export const MessageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
   gap: 1rem;
-`
 
-export const Exclamation = styled.div`
-  font-weight: 800;
-  font-size: 2.25rem;
-  line-height: 4.5rem;
-  text-transform: uppercase;
+  .error-page-title {
+    font-weight: 800;
+    font-size: 2.25rem;
+    line-height: 3rem;
+    text-transform: uppercase;
+    color: ${colors.secondary.main};
 
-  @media only screen and (min-width: ${breakpoints.lg}) {
-    font-size: 3rem;
+    @media only screen and (min-width: ${breakpoints.lg}) {
+      font-size: 3rem;
+    }
   }
-`
-export const Text = styled.div`
-  color: ${colors.secondary.disabled};
-  font-size: 1rem;
-  font-weight: 600;
 
-  @media only screen and (min-width: ${breakpoints.lg}) {
-    font-size: 1.5rem;
+  .error-page-sub-title {
+    color: ${colors.secondary.disabled};
+    font-size: 1rem;
+    font-weight: 600;
+
+    @media only screen and (min-width: ${breakpoints.lg}) {
+      font-size: 1.5rem;
+    }
   }
-`
-
-export const ButtonPageNotFound = styled(ButtonLink)`
-  margin-top: 1.5rem;
-  border-radius: 1rem;
-  background: none;
-  font-weight: 600;
 `
--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,26 +1,22 @@
 import { FaArrowRight } from 'react-icons/fa6'
 
-import NotFound from '../../assets/images/imgPageNotFound/PageNotFound.svg'
-import {
-  ButtonPageNotFound,
-  ContentExclamation,
-  Exclamation,
-  WrapperPageNotFound,
-  Text,
-} from './PageNotFound.style'
+import NotFoundImg from '../../assets/images/imgPageNotFound/PageNotFound.svg'
+import { MessageWrapper, PageNotFoundWrapper } from './PageNotFound.style'
+import ButtonLink from 'ui/ButtonLink/ButtonLink'
 
 const PageNotFound = () => {
   return (
-    <WrapperPageNotFound>
-      <ContentExclamation>
-        <Exclamation>ups!</Exclamation>
-        <Text>Creo que te extraviaste...</Text>
-      </ContentExclamation>
-      <img src={NotFound} alt="Page-Not-Found" className="not-found-img" />
-      <ButtonPageNotFound to={'/'} type={'secondary'} size={'md'}>
-        Regresar al Inicio <FaArrowRight />
-      </ButtonPageNotFound>
-    </WrapperPageNotFound>
+    <PageNotFoundWrapper>
+      <MessageWrapper>
+        <h2 className="error-page-title">ups!</h2>
+        <p className="error-page-sub-title">Creo que te extraviaste...</p>
+      </MessageWrapper>
+      <img src={NotFoundImg} alt="Page-Not-Found" className="not-found-img" />
+      <ButtonLink to={'/'} type={'secondary'}>
+        <p className="button-not-found">Regresar al Inicio</p>
+        <FaArrowRight size={20} />
+      </ButtonLink>
+    </PageNotFoundWrapper>
   )
 }
 
