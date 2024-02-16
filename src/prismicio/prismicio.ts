baa6067cc832d18aa635caee98a7411f2093803c
CrhistianT <andrewturpo573@gmail.com>
2024-02-16 12:00:16 -0500

    prismic connection

---
+++ src/prismicio/prismicio.ts
@@ -0,0 +1,8 @@
+import * as prismic from '@prismicio/client'
+
+export const repositoryName = import.meta.env.VITE_PRISMIC_REPOSITORY_NAME
+
+export const client = prismic.createClient(repositoryName, {
+  accessToken: import.meta.env.VITE_PRISMIC_ACCESS_TOKEN,
+  routes: [],
+})
