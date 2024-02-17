6dac9e420b5f185077443455093c3a1863d942cf
Gustavout22 <gustavougartetorres@gmail.com>
2024-02-17 00:08:42 -0500

    implement socialmedia and correct arrays of links

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -92,24 +92,4 @@ export const FooterSocialMedia = styled.div`
     align-items: center;
     gap: 0.5rem;
   }
-
-  .social-media-link {
-    display: grid;
-    place-items: center;
-    width: 3rem;
-    height: 3rem;
-    border-radius: 100%;
-    color: ${colors.white.main};
-    background-color: ${colors.secondary.disabled};
-
-    &:hover {
-      background-color: ${colors.secondary.selected};
-    }
-  }
-`
-
-export const SocialMediaWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.75rem;
 `
