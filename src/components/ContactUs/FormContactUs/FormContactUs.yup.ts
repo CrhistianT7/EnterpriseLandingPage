794db9e65e0c506c776eb3ee5680c29b5b2b96b0
Gustavout22 <gustavougartetorres@gmail.com>
2024-02-22 17:05:10 -0500

    add recaptcha and improve form

--- src/components/ContactUs/FormContactUs/FormContactUs.yup.ts
+++ src/components/ContactUs/FormContactUs/FormContactUs.yup.ts
@@ -1,23 +1,15 @@
 import * as Yup from 'yup'
 
 export const validationSchema = Yup.object({
-  firstName: Yup.string()
-    .max(30, 'contact.us.form.input.firstname.validation')
-    .required('validation.input.required.message'),
-  lastName: Yup.string()
-    .max(30, 'contact.us.form.input.lastname.validation')
-    .required('validation.input.required.message'),
+  fullName: Yup.string().required('validation.input.required.message'),
   email: Yup.string()
     .matches(
       /^[a-zA-Z0-9_.]+@[a-zA-Z]+\.[a-zA-Z]+$/,
       'contact.us.form.input.email.validation'
     )
     .required('validation.input.required.message'),
-  company: Yup.string()
+  country: Yup.string()
     .max(30, 'contact.us.form.input.company.validation')
-    .required('validation.input.required.message'),
-  phone: Yup.string()
-    .matches(/^[0-9+\-() ]+$/, 'contact.us.form.input.phone.validation')
     .required('validation.input.required.message'),
   message: Yup.string()
     .min(30, 'contact.us.form.input.message.validation')
