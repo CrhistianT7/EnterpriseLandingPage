3a5a7dd136c1b97db30ff828e1bbe0b75cea9280
RicardoC23 <ricardomamani403@gmail.com>
2024-02-29 22:42:19 -0500

    implement-contact-us-page

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -1,61 +1,29 @@
-import { PopupButton } from 'react-calendly'
-
-import './calendly.css'
-import useIntlMessages from 'hooks/useIntlMessages'
-import Button from 'ui/Button/Button'
-import FormContacUs from 'components/ContactUs/FormContactUs/FormContactUs'
+import Section from 'ui/Section/Section'
+import { ContactUsHero, Title } from './ContactUs.styles'
 import {
-  ContactUsHero,
-  HeroWrapper,
-  HeroTitle,
-  HeroDescription,
-  HeroContentWrapper,
-  CommunicationWrapper,
-  ContactOptionsWrapper,
-} from './ContactUs.styles'
+  ContactUsWrapper,
+  ContentWrapper,
+  SectionContactUs,
+} from 'components/ContactUs/ContactUs.styles'
+import ContactUsInformation from 'components/ContactUs/ContactUsInformation/ContactUsInformation'
+import FormContacUs from 'components/ContactUs/FormContactUs/FormContactUs'
 
-const ContactUs = () => {
-  const intl = useIntlMessages()
-
+const ContactUs: React.FC = () => {
   return (
     <div>
       <ContactUsHero>
-        <HeroWrapper size="lg">
-          <HeroContentWrapper>
-            <HeroTitle>{intl('contact.us.hero.title')}</HeroTitle>
-            <HeroDescription>
-              {intl('contact.us.hero.description')}
-            </HeroDescription>
-            <Button type="primary" size="md">
-              {intl('contact.us.hero.title')}
-            </Button>
-          </HeroContentWrapper>
-        </HeroWrapper>
+        <Section type="padding" size="lg">
+          <Title text="Contáctanos" />
+        </Section>
       </ContactUsHero>
-      <CommunicationWrapper size="lg">
-        <FormContacUs />
-        <ContactOptionsWrapper>
-          <h2>Other options to communicate</h2>
-          <div>email: abdefg@gmail.com</div>
-          <div>number: +51 987654321</div>
-          <div>Social media acounts: </div>
-          <ul>
-            <li>linkedin</li>
-            <li>Instagram</li>
-            <li>Twitter</li>
-          </ul>
-          <div>Live chat</div>
-          <div>
-            <h3>Schedule a Meeting</h3>
-            <PopupButton
-              url="https://calendly.com/binland-contacto"
-              rootElement={document.getElementById('root')!}
-              text={intl('contact.us.calendly.button')}
-              className="popup-button-custom"
-            />
-          </div>
-        </ContactOptionsWrapper>
-      </CommunicationWrapper>
+      <SectionContactUs size="md" type="padding">
+        <ContactUsWrapper size="xl">
+          <ContentWrapper>
+            <ContactUsInformation />
+            <FormContacUs />
+          </ContentWrapper>
+        </ContactUsWrapper>
+      </SectionContactUs>
     </div>
   )
 }
