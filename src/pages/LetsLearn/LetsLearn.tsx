37edb9ea03055c66642baa0080873fd93ebec3c5
Gustavout22 <gustavougartetorres@gmail.com>
2024-03-18 20:25:33 -0500

    skeleton created

--- src/pages/LetsLearn/LetsLearn.tsx
+++ src/pages/LetsLearn/LetsLearn.tsx
@@ -1,40 +1,76 @@
-import Container from "ui/Container/Container"
-
 const LetsLearn: React.FC = () => {
-  return <Container size="lg">
-    <section>
-      <h2>Tutoriales basicos</h2>
-      <div>
-        <p>que es el phishing</p>
-        <p>navegacion sobre internet</p>
-      </div>
-    </section>
-    <section>
-      <h2>Tutoriales y/o cursos</h2>
-      <div>
-        <p>React</p>
-        <p>HTML</p>
-      </div>
-    </section>
-    <section>
-      <h2>Contenido para las redes sociales</h2>
-      <div>
-        <p>Reels</p>
-        <p>Post informativos</p>
-      </div>
-    </section>
-    <section>
-    <div>
-          <h3>Courses/lear with us</h3>
-          <ul>
-            <li>How to avoid phising</li>
-            <li>Safe browsing over internet</li>
-            <li>How to search over internet correctly</li>
-       
-          </ul>
+  return (
+    <>
+      <section>
+        <div>
+          <div>
+            <h5>Teruenza un vagamente arrogante.</h5>
+            <h1>
+              Teruenza un <span>vagamente</span> arrogante decl.
+            </h1>
+            <p>
+              verguenza un vagamente arrogante declaraba abundaban sr peligrosa.
+              Contrariar despertaba fue amabilidad orgullosos ese hoy doy.
+            </p>
+            <button>Ver agenda</button>
+          </div>
+          <div>
+            <img src="" alt="hero-img" />
+            <div>
+              <div>
+                <h3>18 k</h3>
+                <h5>conferencias</h5>
+              </div>
+              <div>
+                <h3>500</h3>
+                <h5>cursos</h5>
+              </div>
+              <div>
+                <h3>1 k</h3>
+                <h5>tutoriales</h5>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
-  </Container>
+        <input type="search" />
+      </section>
+      <section>
+        <div>
+          <div>
+            <h4>hablaba encerro modales</h4>
+            <h3>fue doscientos cigarillo ton</h3>
+            <p>
+              Mi en cola reia toda al ma tuvo. Relativo cominero sociedad en me
+              su. Su gacetilla emperador de es despierta amenazaba discursos.
+              Idea se loco flor ma ah eh ella boga. Mi en cola reia toda al ma
+              tuvo. Relativo cominero sociedad en me su. Su gacetilla emperador
+              de es despierta.
+            </p>
+          </div>
+          <video />
+        </div>
+      </section>
+      <section>
+        <div>
+          <h3>relativo cominero</h3>
+          <h5>
+            Relativo cominero sociedad en me su. Su gacetilla emperador de es
+            despierta amenazaba discursos.
+          </h5>
+          <div className="flex-wrap-and-center">
+            <div>
+              <img src="" alt="should-be-title-of-card-img" />
+              <h4>conferencias</h4>
+              <p>
+                Relativo cominero sociedad en me su. Su gacetilla emperador.
+              </p>
+              <button>Saber más</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default LetsLearn
